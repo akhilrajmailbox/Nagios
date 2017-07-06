7c7f58a08400eda9fdc00324f5fba670a47f7d6f
Akhil Raj <akhil@vivasa.in>
2017-07-06 11:18:13 +0530

    updated the scripts for adding new htpassword and users for ui access with specific cfg files

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ docker run command ::
 
 ```
 
-docker run -e MAIL_ADDRESS=<<<mail-address>>> -e HTPASSWORD=<<<password>>> -e HOST_IP=<<<docker-host-ip>>> -p 9999:80 -p 9998:5666 --hostname nagios.local --name nagios.local -it akhilrajmailbox/nagios:latest /bin/bash
+docker run -e MAIL_ADDRESS=<<<mail-address>>> -e HTPASSWORD=<<<password>>> -e HOST_IP=<<<docker-host-ip>>> -p 9999:80 -p 9998:5666 --hostname nagios.local --name nagios.local -v /path/to/folder:/usr/local/nagios/etc/servers -it akhilrajmailbox/nagios:latest /bin/bash
 
 ```
 
@@ -23,9 +23,13 @@ NOTE :::
 
  *	The 'HOST_IP' is optional, if you don't provide, then the server will configure with container ip address.
  *	'hostname' need to provide, if you want a proper name for mail sender
-
+ *	port 80		>>	for web_ui access
+ *	port 5666	>>	for nrpe connection between nagios server and client machines which being monitoring
 
 web_ui ::
+
+http://host-ip:9999/nagios
+
 
 ```
 admin		=		nagiosadmin
@@ -33,6 +37,13 @@ password	=		HTPASSWORD (you need to provide with docker run command)
 
 ```
 
+commands ::
+	for adding another user other than nagiosadmin, use this command inside the docker container 
+```
+nagios-user <<username>> <<password>>
+```
+
 IMPORTANT ::
 
 for using 'check_nrpe' you need to configure in remote machine also, and do not configure with 'argument enable option in remote machine' (security issue)
+
--- compose/start.sh
+++ compose/start.sh
@@ -40,10 +40,12 @@ echo -e '\E[33m'"If you don't want localhost monitoring, It will shows some erro
 echo -e '\E[33m'"you can add new '<<name>>.cfg' file which have hosts and services under '/usr/local/nagios/etc/servers' and reload the service 'service nagios reload' or mount the location to the docker machine with the cfg file $A"
 echo ""
 echo "Configuring........"
-sleep 10
+sleep 5
 
-if [[ ! -f /usr/local/nagios/etc/htpasswd.users ]];then
-if [[ "$HTPASSWORD" = "" ]];then
+if [[ ! -f /usr/local/nagios/etc/nagios-bootsrapped ]];then
+echo "configuring nagios"
+ if [[ ! -f /usr/local/nagios/etc/htpasswd.users ]];then
+  if [[ "$HTPASSWORD" = "" ]];then
 echo ""
 echo -e '\E[32m'"Please provide all environment variable while run the 'docker run command' with -e option $A"
 echo ""
@@ -59,16 +61,16 @@ echo "USERNAME : nagiosadmin"
 echo "PASSWORD : The password you are providing with 'HTPASSWORD' environment variable"
 echo ""
 exit 0
-else
+  else
 sleep 1
-fi
+  fi
 htpasswd -b -c /usr/local/nagios/etc/htpasswd.users nagiosadmin $HTPASSWORD
-fi
+ fi
 
 echo ""
-if [[ "$HOST_IP" = "" ]]; then
+    if [[ "$HOST_IP" = "" ]]; then
 HOST_IP=`ifconfig | grep -A 1 eth0 | tail -1 | cut -d ":" -f 2 | cut -d " " -f 1`
-fi
+    fi
 
 cat <<EOF > /etc/xinetd.d/nrpe
 # default: on
@@ -89,18 +91,39 @@ service nrpe
 }
 EOF
 
+     if [[ ! -f /usr/local/bin/nagios-user ]];then
+cat <<EOF > /usr/local/bin/nagios-user
+#!/bin/bash
+htpasswd -b /usr/local/nagios/etc/htpasswd.users \$1 \$2
+EOF
+     fi
+chmod 777 /usr/local/bin/nagios-user
+
+
 sed -i "s|NOTIFY_MAIL_ADDRESS|$MAIL_ADDRESS|g" /usr/local/nagios/etc/objects/commands.cfg
 
-if [[ $LOCAL_MONITOR == "y" || $LOCAL_MONITOR == "Y" ]]
+     if [[ $LOCAL_MONITOR == "y" || $LOCAL_MONITOR == "Y" ]]
 then
 echo ""
 else
 sed -i "s|cfg_file=/usr/local/nagios/etc/objects/localhost.cfg|#cfg_file=/usr/local/nagios/etc/objects/localhost.cfg|g" /usr/local/nagios/etc/nagios.cfg
-fi
+     fi
 
 postconf -e myhostname="`hostname -f`"
 postconf -e mydestination="`hostname -f`, localhost.localdomain, localhost"
 echo "`hostname -f`" > /etc/mailname
+
+cp /root/monitor.cfg-reference /usr/local/nagios/etc/servers/monitor.cfg-reference
+cp /root/specific-monitor.cfg-reference /usr/local/nagios/etc/servers/specific-monitor.cfg-reference
+cp /root/specific-template.cfg-reference /usr/local/nagios/etc/servers/specific-template.cfg-reference
+
+touch /usr/local/nagios/etc/nagios-bootsrapped
+
+else
+echo ""
+echo "nagios already configured....."
+echo ""
+fi
 
 service xinetd restart & wait
 service apache2 restart & wait
@@ -112,16 +135,20 @@ service xinetd restart & wait
 service nagios restart & wait
 service apache2 restart & wait
 service postfix restart & wait
+chown -R nagios:nagios /usr/local/nagios/etc/servers
 
 echo "##################################"
 echo ""
-echo -e '\E[32m'"The config file should be under '/usr/local/nagios/etc/servers/' with '.cfg' extension $A"
-echo -e '\E[32m'"You can find a reference file 'monitor.cfg-reference' under '/usr/local/nagios/etc/servers/' $A"
+echo -e '\E[33m'"The config file should be under '/usr/local/nagios/etc/servers/' with '.cfg' extension $A"
+echo ""
+echo -e '\E[33m'"You can find a reference file 'monitor.cfg-reference' under '/usr/local/nagios/etc/servers/' for simple configuration $A"
+echo -e '\E[32m'"You can find a reference file 'specific-monitor.cfg-reference' and 'specific-template.cfg-reference' under '/usr/local/nagios/etc/servers/' for specific user access configuration $A"
 echo -e '\E[32m'"Configure your config file and reload the server with 'service nagios reload' $A"
 echo ""
 echo "##################################"
+echo ""
+echo -e '\E[33m'"Use command 'nagios-user' for creating more user than nagiosadmin $A"
+echo ""
 echo "##################################"
-cp /root/monitor.cfg-reference /usr/local/nagios/etc/servers/monitor.cfg-reference
-chown -R nagios:nagios /usr/local/nagios/etc/servers
-#tailf /usr/local/nagios/var/nagios.log
+echo "##################################"
 tailf /root/start.sh
